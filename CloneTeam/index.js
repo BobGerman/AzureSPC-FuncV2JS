fc8c0b1cc16f3bc8e477e40c1a076d44e30ce1f8
Bob German <bgerman1@hotmail.com>
2019-05-12 19:10:38 -0400

    Remove experimental function

--- CloneTeam/index.js
+++
@@ -1,9 +0,0 @@
-module.exports = async function (context, myQueueItem) {
-    context.log('JavaScript queue trigger function processed work item', myQueueItem);
-    try {
-        context.bindings.myOutputQueueItem = ["message 1","message 2"];
-    } catch (ex) {
-        context.log(`Error: ${ex}`);
-    }
-    context.done();
-};
